fd033531a962468546e839a2ec3dfc32ad61e1d2
jrheard <jrheard@cs.stanford.edu>
2013-01-12 19:38:28 -0800

    okay, now we're filtering for only transactions that happened in 2012

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.csv "0.1.2"]])
+                 [org.clojure/data.csv "0.1.2"]
+                 [clj-time "0.4.4"]])
--- src/finance/core.clj
+++ src/finance/core.clj
@@ -1,7 +1,10 @@
 (ns finance.core
   "Tells me how much money I spend per year vs how much money I earn per year."
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clj-time.coerce :refer [to-long]]
+            [clj-time.core :refer [date-time]]
+            [clj-time.format :refer [parse formatter]]))
 
 (defn get-transactions []
   (let [lines (with-open [f (io/reader (io/resource "transactions.csv"))]
@@ -10,5 +13,18 @@
         lines (rest lines)]
     (reduce #(conj % (zipmap header %2)) [] lines)))
 
+(defn parse-date
+  "Takes a string like '1/11/2013', returns a DateTime instance."
+  [date-str]
+  (parse (formatter "MM/dd/yyyy") date-str))
+
+(defn valid-date
+  "Returns true if the specified date happened in 2012, false otherwise."
+  [date]
+  (apply < (map to-long [(date-time 2012 01 01)
+                         (parse-date date)
+                         (date-time 2012 12 31)])))
+
 (defn -main [& args]
-  (prn (get-transactions)))
+  (let [transactions (filter #(valid-date (% "Date"))(get-transactions))]
+    (prn (sort (map #(% "Date") transactions)))))
